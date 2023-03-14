6bb0ec56dc1d680d52f63ff05bc09a172fa203b9
金戈宁 <626723063@qq.com>
2023-03-14 20:12:53 +0800

    fix: tijiaao xxxx

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -6,6 +6,7 @@ import type { RouteObject } from 'react-router-dom';
 import React from 'react';
 // 懒加载的方式引入对应页面 配置suspense在index.tsx
 import { lazy } from 'react';
+// xxxxx
 // icons
 import {
   CopyOutlined,
