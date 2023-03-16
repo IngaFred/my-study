d76967580491ff4e089c48b989c5b0a140d077dd
IngaFred <116004221+IngaFred@users.noreply.github.com>
2023-03-16 14:01:43 +0800

    feat: 第三节课ts welcomeIIIrcc组件编写

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import styles from "./Home.module.scss";
 // 进行二级路由测试
 import { Outlet } from "react-router-dom";
+import WelcomeIIIIrcc from "../../components/test/WelcomeIIIIrcc";
 
 export default function () {
   return (
     <div>
       Home
+      <WelcomeIIIIrcc name={"MyPropsName"}></WelcomeIIIIrcc>
       <Outlet />
     </div>
   );
